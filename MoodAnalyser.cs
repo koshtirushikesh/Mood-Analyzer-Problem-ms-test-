d4fbc6c461a124137c9d2c3d6189e7b4e4fbb759
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-01-05 09:43:16 +0530

    ability to analyse and respond Happy or Sad Mood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyser
+    {
+        public static string MoodAnalyserPatternOfSad = "sad";
+        public static string MoodAnalyserPatternOfHappy = "happy";
+
+        public string AnalyzeMood(string MoodInput) 
+        {
+            if (Regex.IsMatch(MoodInput, MoodAnalyserPatternOfSad, RegexOptions.IgnoreCase))
+            {
+                return "sad";
+
+            }
+            else if (Regex.IsMatch(MoodInput, MoodAnalyserPatternOfHappy, RegexOptions.IgnoreCase))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "I think you are happy";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How are you feeling today ");
+            string mood =  Console.ReadLine();
+
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            Console.WriteLine(moodAnalyser.AnalyzeMood(mood));
+        }
+    }
+}
