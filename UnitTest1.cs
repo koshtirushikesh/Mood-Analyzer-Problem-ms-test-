2e415032643f265254e1d30e3570aa659dce6421
koshtirushikesh <117973034+koshtirushikesh@users.noreply.github.com>
2023-01-06 16:55:58 +0530

    Test Case

---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+using MoodAnalyzerProblem;
+
+namespace TestMoodAnalyserProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange 
+            string mood = "I am in Sad Mood";
+            string expexctedoutput = "sad";
+
+            MoodAnalyser obj = new MoodAnalyser();
+            // Act
+            string actualoutput = obj.AnalyzeMood(mood);
+            //Arrang
+            Assert.AreEqual(expexctedoutput, actualoutput);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Arrange 
+            string mood = "I am in happy Mood";
+            string expexctedoutput = "sad";
+
+            MoodAnalyser obj = new MoodAnalyser();
+            // Act
+            string actualoutput = obj.AnalyzeMood(mood);
+            //Arrang
+            Assert.AreEqual(expexctedoutput, actualoutput);
+        }
+    }
+}
